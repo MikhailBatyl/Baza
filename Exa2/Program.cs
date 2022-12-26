51fa5bbc3991a4f426a2d51a0c1ec36bc1da931a
MikhailBatyl <batyl1990@mail.ru>
2022-12-26 23:19:37 +0700

    Код

---
+++ Exa2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите текст:");
+string text = Console.ReadLine();
+string [] Variable = text.Split(' ');
+Console.WriteLine("меньше трех: ");
+
+for (int i = 0; i < Variable.Length; i++)
+{
+    if (Variable[i].Length < 4)
+    {
+        Console.Write($"[{Variable[i]}] ");
+    }
+}
